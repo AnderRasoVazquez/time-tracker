e1fa84e6beb5fedc47fb45aea33e3e3c8981358b
Ander Raso Vázquez <anderraso@gmail.com>
2018-11-08 20:41:32 +0100

    main script added
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- timetracker/__main__.py
+++ timetracker/__main__.py
@@ -1,6 +1,12 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from ui.window import TrackerWindow
+
 
 def main():
-    print("prueba")
+    TrackerWindow()
+    Gtk.main()
 
 
 if __name__ == '__main__':
