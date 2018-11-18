a6f92420df409ac487fcd2be4d683b1592e27739
Ander Raso Vázquez <anderraso@gmail.com>
2018-11-18 21:48:21 +0100

    translation global function added
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- timetracker/__main__.py
+++ timetracker/__main__.py
@@ -1,8 +1,14 @@
 import sys
+import pandas  # TODO without this here I get the following error:
+               # from pandas.util._move import ( ValueError: module functions cannot set METH_CLASS or METH_STATIC
 from application import Application
+
+import os
+import gettext
+from definitions import ROOT_DIR
+
+gettext.install('base', localedir=os.path.join(ROOT_DIR, 'locales'))
 
 if __name__ == "__main__":
     app = Application()
     app.run(sys.argv)
-
-# TODO: gettext https://phraseapp.com/blog/posts/translate-python-gnu-gettext/
